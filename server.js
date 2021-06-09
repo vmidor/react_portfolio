6eda673ad1361880e1df6390e08cee833a89756f
vmidor <76964172+vmidor@users.noreply.github.com>
2021-06-09 10:27:58 -0400

    react app created

--- server.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const app = express()
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static('/'));
-
-app.listen(PORT, () => console.log('listening on PORT', PORT));
